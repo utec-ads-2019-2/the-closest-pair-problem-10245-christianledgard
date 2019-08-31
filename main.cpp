35647a1c8f7a805717e03e25e5112eff010a0593
Christian Ledgard <39962750+christianledgard@users.noreply.github.com>
2019-08-31 16:30:25 -0500

    Solución

--- main.cpp
+++ main.cpp
@@ -6,43 +6,32 @@
 
 using namespace std;
 
-double distanciaDosPuntos(int x1, int y1, int x2, int y2){
-    return sqrt( pow(x2-x1,2)  + pow(y2-y1,2));
+long double distanciaDosPuntos(pair<long double,long double>p1,pair<long double,long double>p2){
+    return sqrt(pow(p1.first-p2.first,2)  + pow(p1.second-p2.second,2));
 }
 
-
 int main() {
-
     int n;
     while(cin>>n){
         if(n == 0) continue;
-        int puntos[n][2], punto;
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < 2; ++j) {
-                cin >> punto;
-                puntos[i][j] = punto;
-            }
-        }
+        long double distanciaMinima = DBL_MAX;
 
-        double menor = DBL_MAX;
+        pair<long double, long double> Puntos[n];
 
-        for (int l = 0; l < n; ++l) {
-            for (int k = 0; k <= n; ++k) {
-                if (puntos[l][0] != puntos[k][0] and puntos[l][1] !=  puntos[k][1]){
-                    auto dis = distanciaDosPuntos(puntos[l][0],puntos[l][1],puntos[k][0],puntos[k][1]);
-                    if(dis < menor  and dis != 0){
-                        menor = dis;
-                    }
-                }
-            }
-        }
+        for (int i = 0; i < n; ++i)
+            cin >> Puntos[i].first >> Puntos[i].second;
 
-        if(menor<10000){
-            cout << fixed << setprecision(4) << menor << endl;
-        }else{
+        for (int l = 0; l < n; ++l)
+            for (int k = l + 1; k < n; ++k)
+                distanciaMinima = min(distanciaDosPuntos(Puntos[l],Puntos[k]),distanciaMinima);
+
+
+        if(distanciaMinima<10000)
+            cout << fixed << setprecision(4) << distanciaMinima << endl;
+        else
             cout << "INFINITY" << endl;
-        }
+
     }
     return 0;
 }
